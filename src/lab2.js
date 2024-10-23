d139f1bcb7ae1e0371ae461c81302d745817a143
dkowalski1906 <134270198+dkowalski1906@users.noreply.github.com>
2024-10-23 20:55:47 +0200

    almost done - DKOW
    lab1 à 3

--- src/lab2.js
+++ src/lab2.js
@@ -15,3 +15,33 @@
   -- Hint, use postman to interact with the server 
   
  */
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
+  const { name, age } = req.body;
+
+  res.send(`Received POST request. Name: ${name}, Age: ${age}`);
+});
+
+app.delete("/delete", (req, res) => {
+  console.log(req.body);
+
+  const { name, age } = req.body;
+
+  res.send(`Received POST request. Name: ${name}, Age: ${age}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
